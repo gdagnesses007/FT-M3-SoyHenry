9a1ce9ad9d05c5f1ce84e32fca81e06b16d5e05f
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-08-11 18:23:25 -0600

    . testing Promise.all()

--- 02-Promises/demo/encadenandoPromesas.js
+++ 02-Promises/demo/encadenandoPromesas.js
@@ -1,47 +1,51 @@
-var primerMetodo = function() {
-   var promise = new Promise(function(resolve, reject){
-      setTimeout(function() {
+var primerMetodo = function () {
+   var promise = new Promise(function (resolve, reject) {
+      setTimeout(function () {
          console.log('Terminó el primer método');
-         resolve({num: '123'}); //pasamos unos datos para ver como los manejamos
+         resolve({ num: '123' }); //pasamos unos datos para ver como los manejamos
       }, 2000); // para simular algo asincronico hacemos un setTimeOut de 2 s
    });
    return promise;
 };
- 
- uno.then(function(data) {
+
+/* uno.then(function (data) {
    return segundoMetodo(data);
- }).then(function(data) {
+}).then(function (data) {
    return tercerMetodo(data);
- }).then(function(data) {
+}).then(function (data) {
    console.log(data);
- })
+}) */
 
 
-var segundoMetodo = function(datos) {
-   var promise = new Promise(function(resolve, reject){
-      setTimeout(function() {
+var segundoMetodo = function (datos) {
+   var promise = new Promise(function (resolve, reject) {
+      setTimeout(function () {
          console.log('Terminó el segundo método');
-         resolve({nuevosDatos: datos.num + ' concatenamos texto y lo pasamos'});
+         resolve({ nuevosDatos: /* datos.num +  */' concatenamos texto y lo pasamos' });
       }, 2000);
    });
    return promise;
 };
- 
-var tercerMetodo = function(datos) {
-   var promise = new Promise(function(resolve, reject){
-      setTimeout(function() {
+
+var tercerMetodo = function (datos) {
+   var promise = new Promise(function (resolve, reject) {
+      setTimeout(function () {
          console.log('Terminó el tercer método');
-         console.log(datos.nuevosDatos); //imprimos los datos concatenados
+        /*  console.log(datos.nuevosDatos);  *///imprimos los datos concatenados
          resolve('hola');
       }, 3000);
    });
    return promise;
 };
- 
-primerMetodo()
+
+/* primerMetodo()
    .then(segundoMetodo)
    .then(tercerMetodo)
-   .then(function(datos){
+   .then(function (datos) {
       console.log(datos); //debería ser el 'hola' que pasamos en tercerMetodo
-   });
+   }); */
+
+Promise.all([primerMetodo(), segundoMetodo(), tercerMetodo()])
+   .then(resultado => console.log('resultado: ', resultado))
+   .catch(error => console.log('error', error))
 
