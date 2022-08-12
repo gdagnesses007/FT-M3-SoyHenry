a0e7790bf759416e62760bf7900cfe6c16c0dacc
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-08-12 17:36:05 -0600

    ejercicio 1 (showByName/app.js) . completo . se hicieron algunas config en el package.json

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,10 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+    const { url } = req
+    const img = fs.readFileSync(`${__dirname}/images/${url}.jpg`)
+    res.end(img)
+}).listen(1337, '127.0.0.1')
