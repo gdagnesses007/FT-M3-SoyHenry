64e53313fa5be8b04096198d9d73d560e3f1a65d
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-08-13 22:01:45 -0600

    ejercicio 2b . completo . hay un detalle al usar la funcion .replace(), se esta reemplazando solo la primera ocurrencia

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -29,22 +29,65 @@ http.createServer((req, res) => {
 
     const { url } = req
 
-    if (/^\/api\/?$/.test(url)) {           //url: /api | /api/
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(beatles))
+    if (url === '/') {
+        fs.readFile('./index.html', (err, data) => {
+            if (err) {
+                errorResponse(res, err.toString())
+            } else {
+                htmlResponse(res, data)
+            }
+        })
+    } else if (/^\/api\/?$/.test(url)) {           //url: /api | /api/
+        jsonResponse(res, beatles)
     } else if (/^\/api\/.+/.test(url)) {    //url: /api/*
-        const name = url.split('/').pop()
-        const item = beatles.find(item => encodeURI(item.name) === name)
+        const item = getArgUrl(url)
         if (item === undefined) {
-            res.writeHead(404)
-            res.end("The person you're looking for does not exists in database")
+            errorResponse(res, "The person you're looking for does not exists in database")
         } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(item))
+            jsonResponse(res, item)
         }
+    } else if (/^\/.*$/.test(url)) {
+        const item = getArgUrl(url)
+        fs.readFile('./beatle.html', 'utf8', (err, data) => {
+            if (err) {
+                errorResponse(res, err.toString())
+            } else if (item === undefined) {
+                errorResponse(res, "The person you're looking for does not exists in database")
+            } else {
+                for (const key in item) {
+                    data = data.replace(`{%${key}%}`, () => item[key])
+                }
+                htmlResponse(res, data)
+            }
+        })
     } else {
-        res.writeHead(404)
-        res.end("Ups!!! Something has been broken!!!")
+        errorResponse(res, "Ups!!! Something has been broken!!!")
     }
 
 }).listen(1337, 'localhost')
+
+/**
+ * Devuelve el objeto que coincide con el ultimo argumento en la url
+ * 
+ * @param {string} url 
+ * @returns Objeto encontrado o undefined en caso contrario
+ */
+const getArgUrl = url => {
+    const name = url.split('/').pop()
+    return beatles.find(item => encodeURI(item.name) === name)
+}
+
+const errorResponse = (res, msg = '') => {
+    res.writeHead(404)
+    res.end(msg)
+}
+
+const jsonResponse = (res, json) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(json))
+}
+
+const htmlResponse = (res, html) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(html)
+}
