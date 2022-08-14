7cdde498f3a16070138f013b3ba6581245723815
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-08-14 14:50:05 -0600

    ejercicio 1 . fulfilled

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,7 +4,32 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+const PENDING_STATE = 'pending'
+const FULFILLED_STATE = 'fulfilled'
+const REJECTED_STATE = 'rejected'
 
+function $Promise(executor) {
+    if (typeof (executor) !== 'function') {
+        throw new TypeError(/executor.+function/i)
+    }
+    this._state = PENDING_STATE
+    this._value = undefined
+    executor(this._internalResolve.bind(this), this._internalReject.bind(this))
+}
+
+$Promise.prototype._internalResolve = function (data) {
+    if (this._state === PENDING_STATE) {
+        this._value = data
+        this._state = FULFILLED_STATE
+    }
+}
+
+$Promise.prototype._internalReject = function (data) {
+    if (this._state === PENDING_STATE) {
+        this._value = data
+        this._state = REJECTED_STATE
+    }
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
