24983185137ecedb561a3183c42ebdb0a9d02279
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-08-15 09:32:06 -0600

    ejercicio 2 . fulfilled

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -14,6 +14,7 @@ function $Promise(executor) {
     }
     this._state = PENDING_STATE
     this._value = undefined
+    this._handlerGroups = []
     executor(this._internalResolve.bind(this), this._internalReject.bind(this))
 }
 
@@ -22,6 +23,7 @@ $Promise.prototype._internalResolve = function (data) {
         this._value = data
         this._state = FULFILLED_STATE
     }
+    this._callHandlers()
 }
 
 $Promise.prototype._internalReject = function (data) {
@@ -29,6 +31,29 @@ $Promise.prototype._internalReject = function (data) {
         this._value = data
         this._state = REJECTED_STATE
     }
+}
+
+$Promise.prototype.then = function (successCb, errorCb) {
+    const handler = {
+        successCb: typeof (successCb) === 'function' ? successCb : false,
+        errorCb: typeof (errorCb) === 'function' ? errorCb : false
+    }
+    this._handlerGroups.push(handler)
+    this._callHandler(handler)
+}
+
+$Promise.prototype._callHandler = function (handler) {
+    if (this._state === FULFILLED_STATE) {
+        handler.successCb(this._value)
+    } else if (this._state === REJECTED_STATE) {
+        handler.errorCb(this._value)
+    }
+}
+
+$Promise.prototype._callHandlers = function () {
+    this._handlerGroups.forEach(handler => {
+        this._callHandler(handler)
+    })
 }
 
 module.exports = $Promise;
